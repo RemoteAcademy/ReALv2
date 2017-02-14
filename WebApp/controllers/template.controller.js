03545499e65cc568beb65aa7e995ba8d2dc48a68
Cole Baxter <baxtec2@rpi.edu>
2017-02-14 16:43:08 -0500

    Initial webserver

---
+++ WebApp/controllers/template.controller.js
@@ -0,0 +1,10 @@
+/**********************************
+ * File: app.controller.js        *
+ * Purpose: Top-Level Controller  *
+ **********************************/
+
+angular.
+  module( 'realWebapp', [ ] ).
+  controller( 'realWebappCtrl', function( $scope ) {
+    $scope.msg = "Hello Foobar!";
+  });
